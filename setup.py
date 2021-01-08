124c88303352e1fc774d3a8ff5aad8cb0dc9b35f
Michele Mastropietro <michele.mastropietro@gmail.com>
2021-01-08 11:45:25 +0100

    fix scripts name

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
+import os
 from setuptools import setup
 import glob
-scripts = glob.glob('ngc1427apaper/plots/*')
+
+scripts = glob.glob('ngc1427apaper/plots/plot_*')
 
 setup(name='ngc1427apaper',
-      version='0.1',
+      version='0.1.1',
       description='Package for NGC1427A Mastropietro et al 2020 paper',
       license="MIT",
       author='Michele Mastropietro',
       author_email='michele.mastropietro@ugent.be',
-      install_requires=['astropy', 'pandas', 'matplotlib', 'scipy', 'numpy',
-                        # 'pynbody', 'numpy-quaternion',
-                        'scikit-image', 'numba', 'tqdm', 'photutils'],
+      install_requires=['simulation', 'astropy', 'pandas', 'matplotlib', 'scipy', 'numpy',
+                        'pynbody',
+                        'scikit-image', 'tqdm', 'photutils'],
       packages=['ngc1427apaper', 'ngc1427apaper.plots'],
       package_data={'ngc1427apaper': ['data/*']},
       scripts=scripts,
       entry_points={}
-          # 'console_scripts': ['sim_duration=simulation.sim_duration:main',]
-                   # }
       )
