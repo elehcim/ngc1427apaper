690088b88b1f74c35a939fd5c78dd95872cafe25
Michele Mastropietro <michele.mastropietro@gmail.com>
2021-04-06 14:43:17 +0200

    fix run_chuncks

--- ngc1427apaper/pipeline/produce_quest/run_chunks.sh
+++ ngc1427apaper/pipeline/produce_quest/run_chunks.sh
@@ -1,3 +1,3 @@
-for i in `seq 0 19`; do
-	python morphological_quest_script_multi_iso_hi.py --cache-file=../cache_with_iso.pkl $i
+for i in `seq 0 38`; do
+	python morphological_quest_script_multi_iso_hi.py --cache-file=../cache_with_iso.pkl $i -s 2000
 done
